3eca6ba98368245791bd1d0b104fe8905e21329e
Talita Rodrigues <talitarodriguesdefaria1203@gmail.com>
2022-04-10 20:40:20 -0300

    Finalização da validação dos campos obrigatórios e validação do campo idade

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ var nome = document.querySelector("[name='nomeUsuario']")
 var dataNascimento = document.querySelector("[name='dataNascimento']")
 var cpf = document.querySelector("[name='cpf']")
 var cep = document.querySelector("[name='endereco.cep']")
-var largadouro = document.querySelector("[name='endereco.logradouro']")
+var logradouro = document.querySelector("[name='endereco.logradouro']")
 var numero = document.querySelector("[name='endereco.numero']")
 var bairro = document.querySelector("[name='endereco.bairro']")
 var complemento = document.querySelector("[name='endereco.complemento']")
@@ -16,12 +16,53 @@ var checkBox = document.querySelector("[name='checkBox']")
 function validarCampos(){
     if(nome.value == undefined || nome.value == null || nome.value.length == 0 ){ 
         alert('O campo "nome" não foi informado.')
+        return false
     }else if(dataNascimento.value == undefined || dataNascimento.value == null || dataNascimento.value.length == 0){
         alert('O campo "Data de nascimento" não foi informado.')
+        return false
     }else if(cpf.value == undefined || cpf.value == null || cpf.value.length == 0){
         alert('O campo "CPF" não foi informado.')
-    }else if(){
-        
+        return false
+    }else if(cep.value == undefined || cep.value == null || cep.value.length == 0){
+        alert('O campo "CEP" não foi informado.')
+        return false
+    }else if(logradouro.value == undefined || logradouro.value == null || logradouro.value.length == 0){
+        alert('O campo "Largadouro" não foi informado.')
+        return false
+    }else if(numero.value == undefined || numero.value == null || numero.value.length == 0){
+        alert('O campo "Número" não foi informado.')
+        return false
+    }else if(bairro.value == undefined || bairro.value == null || bairro.value.length == 0){
+        alert('O campo "Bairro" não foi informado.')
+        return false
+    }else if(cidade.value == undefined || cidade.value == null || cidade.value.length == 0){
+        alert('O campo "Cidade" não foi informado.')
+        return false
+    }else if(estado.value == undefined || estado.value == null || estado.value.length == 0){
+        alert('O campo "Estado" não foi informado.')
+        return false
+    }else if(telefoneCelular.value == undefined || telefoneCelular.value == null || telefoneCelular.value.length == 0){
+        alert('O campo "Telefone Celular" não foi informado.')
+        return false
     }
+    return true 
+}
 
+function validarIdade(){
+    var dataAtual = new Date()
+    var anoDigitado = dataNascimento.value.substr(0,4)
+    var anoAtual = dataAtual.getFullYear()
+    var idade = Number(anoAtual)-Number(anoDigitado)
+    if(idade < 18){
+        alert('Cadastro permitido somente para maiores de idade')
+        return
+    }
+}
+
+function cadastrar(){
+    var campos = validarCampos()
+    if(!campos){
+        return 
+    }
+    validarIdade()
 }
